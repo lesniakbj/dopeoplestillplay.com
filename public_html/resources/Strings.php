31d67e90334ca829908b340bbd529ddc3510fb93
Brendan L <brendan.lesniak@gmail.com>
2015-07-29 19:59:25 -0400

    Changed from static to public variable

--- public_html/resources/Strings.php
+++ public_html/resources/Strings.php
@@ -1,6 +1,7 @@
 <?php
 class ResourceStrings {
-	public static $homeStrings = array(
+	public $homeStrings = array(
 			"CONSTRUCTION_TEXT" => "Enter your email address below to get updates on release, ask any questions related to the project, or to volunteer to help on the project."
 		);
 }
+?>
