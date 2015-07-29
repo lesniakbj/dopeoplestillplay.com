59a69c07544ecac96a080d7a85db79f7fd6d3b60
Brendan L <brendan.lesniak@gmail.com>
2015-07-29 16:21:55 -0400

    Adding project files as initial commit

---
+++ code_igniter/application/views/templates/footer.php
@@ -0,0 +1,8 @@
+			<div class="row">
+				<div class="one-third column">&nbsp;</div>
+				<em class="one-third column">Created By: Brendan Lesniak &copy; 2015</em>
+				<div class="one-third column">&nbsp;</div>
+			</div>
+		</div>
+        </body>
+</html>
