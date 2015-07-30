04504c9f4c5aa247535d2a9bea548aa858079265
Brendan Lesniak <blesniak@denniskirk.com>
2015-07-30 11:21:03 -0500

    Added some data to see if that was the reason for the error

--- public_html/igniter/application/controllers/Form_Handler.php
+++ public_html/igniter/application/controllers/Form_Handler.php
@@ -4,8 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Form_Handler extends CI_Controller {
 
 	public function email_form {
-		$this->load->view('templates/header.php', $data);
-			echo <h1>HELLO!</h1>
-		$this->load->view('templates/footer.php', $data);
+		$data['title'] = 'Form Submission';
+	
+		$this->load->view('templates/header', $data);
+		$this->load->view('templates/footer', $data);
 	}
 }
