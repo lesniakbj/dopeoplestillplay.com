fc0abbf0bbdd78f8ba57e7af3ba0e0f771020c65
Brendan Lesniak <blesniak@denniskirk.com>
2015-07-30 11:22:38 -0500

    Changed data again

--- public_html/igniter/application/controllers/Form_Handler.php
+++ public_html/igniter/application/controllers/Form_Handler.php
@@ -3,9 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Form_Handler extends CI_Controller {
 
-	public function email_form {
-		$data['title'] = 'Form Submission';
-	
+	public function emailform {
 		$this->load->view('templates/header', $data);
 		$this->load->view('templates/footer', $data);
 	}
