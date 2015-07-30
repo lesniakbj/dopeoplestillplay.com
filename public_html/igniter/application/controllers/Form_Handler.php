a356f084f10b3c6e84a24cf7e7b259e8ba3da72f
Brendan Lesniak <blesniak@denniskirk.com>
2015-07-30 11:23:59 -0500

    Forgot () on method name, so it was telling me I was missing { or }. After adding this problem is gone

--- public_html/igniter/application/controllers/Form_Handler.php
+++ public_html/igniter/application/controllers/Form_Handler.php
@@ -3,7 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Form_Handler extends CI_Controller {
 
-	public function emailform {
+	public function email_form() {
+		$data['title'] = 'Email Submission';
+	
 		$this->load->view('templates/header', $data);
 		$this->load->view('templates/footer', $data);
 	}
