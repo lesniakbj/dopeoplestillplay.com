177320bf27971aa112056fc4a20d2365848dcf66
Brendan Lesniak <blesniak@denniskirk.com>
2015-07-30 11:24:52 -0500

    Added css variable

--- public_html/igniter/application/controllers/Form_Handler.php
+++ public_html/igniter/application/controllers/Form_Handler.php
@@ -5,6 +5,7 @@ class Form_Handler extends CI_Controller {
 
 	public function email_form() {
 		$data['title'] = 'Email Submission';
+		$data['css'] = '';
 	
 		$this->load->view('templates/header', $data);
 		$this->load->view('templates/footer', $data);
