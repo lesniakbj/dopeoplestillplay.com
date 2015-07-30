e6b9f483cfc3a203077267d25cda2cf97cb150e4
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-07-30 12:10:59 -0400

    Added meta strings to the String resources file. These are echoed when the meta tags are created

--- public_html/resources/Strings.php
+++ public_html/resources/Strings.php
@@ -1,7 +1,12 @@
 <?php
 class ResourceStrings {
+	public static $metaStrings = array(
+		"DESCRIPTION" => "Do people still play the games you love? Find out here!",
+		"KEYWORDS" => "games, online, mmo, rpg, shooter, search, library, ratings"
+	);
+	
 	public static $homeStrings = array(
 			"CONSTRUCTION_TEXT" => "Enter your email address below to get updates on release, ask any questions related to the project, or to volunteer to help on the project."
-		);
+	);
 }
 ?>
