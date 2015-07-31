4985d9d894b2050dde8101d5e18d19c53f899453
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-07-31 14:35:42 -0400

    Changed controller to handle emails and use correct calling of methods. Next, move DB stuff to a FormEmailModel

--- public_html/igniter/application/controllers/FormHandler.php
+++ public_html/igniter/application/controllers/FormHandler.php
@@ -5,7 +5,7 @@ class FormHandler extends CI_Controller {
 
 	public function handle($formName) {
 		// Check which form was submitted:
-		if($formName = 'construction-email') {
+		if($formName = 'Submit Email') {
 			// Get the user input from the GET or POST request.
 			$this->handleEmailRequest($this->input->get_post('user-email', TRUE));
 		}
@@ -14,13 +14,33 @@ class FormHandler extends CI_Controller {
 	private function handleEmailRequest($requestParams) {
 		// Since this is an email, the requestParams will be an email address
 		$email = $requestParams;
+		
+		// Submit the email to MySQL database for safekeeping
+		$this->submitEmailToDatabase($email);
+		
+		// Display the confirmation page
+		$this->displayConfirmationPage('Email Submission', $email);
+		
+		// After showing the confirmation page, redirect back the home page.
+		$this->load->helper('url');
+		$this->output->set_header('refresh:8;url=/');
+	}
+	
+	private function submitEmailToDatabase($email) {
+		// TO DO - Move DB calls to the model, load the model and call it here.
+		$this->load->database();
+		$sql = "INSERT INTO user_email_list (email_address) VALUES(".$this->db->escape($email).")";
+		$query = $this->db->query($sql);
+	}
+	
+	private function displayConfirmationPage($pageTitle, $emailAddress) {
 		// Required page data, either leave blank or add specific entries here
-		$data['title'] = 'Email Submission';
-		$data['css'] = '';
+		$data['title'] = $pageTitle;
+		$data['email'] = $emailAddress;
 	
 		// Load the templates, and echo the data we got in the middle. 
 		$this->load->view('templates/header', $data);
-		echo $email;
+		$this->load->view('statics/email_confirmation', $data);
 		$this->load->view('templates/footer', $data);
 	}
 }
