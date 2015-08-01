94ad6fd3992e9fae1a9741288d07743ee7b670cc
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-08-01 12:52:30 -0400

    Changing URL base in the config cause the resources to not be loaded any more, need to but them to the base of the new document root. Images, CSS, other resources needed.

---
+++ public_html/admin/resources/Strings.php
@@ -0,0 +1,12 @@
+<?php
+class ResourceStrings {
+	public static $metaStrings = array(
+		"DESCRIPTION" => "Do people still play the games you love? Find out here!",
+		"KEYWORDS" => "games, online, mmo, rpg, shooter, search, library, ratings"
+	);
+	
+	public static $homeStrings = array(
+			"CONSTRUCTION_TEXT" => "Enter your email address below to get updates on the site's progress, ask any questions related to the project, or to volunteer some time to the project. We are working hard and are excited to help you determine if people are still playing your favorite games!"
+	);
+}
+?>
