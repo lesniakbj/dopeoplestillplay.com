0be3912781fac616f59cffe6807070e7eb05fd23
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-08-01 09:56:43 -0500

    Added some row behavior, trying to center content

--- public_html/igniter/application/views/statics/admin_home.php
+++ public_html/igniter/application/views/statics/admin_home.php
@@ -1,10 +1,22 @@
 <form name="admin-login" class="" action="" method="post">
 	<div class="row">
-		<label for="admin-username">Admin Username:</label>
-		<input name="admin-username" type="text">
+		<div class="row">
+			<div class="one-third columns">&nbsp;</div>
+			<div class="one-third columns">
+				<label "for="admin-username">Admin Username:</label>
+				<input name="admin-username" type="text">
+			</div>
+			<div class="one-third columns">&nbsp;</div>
+		</div>
 		<?php echo br(1); ?>
-		<label for="admin-password">Admin Password:</label>
-		<input name="admin-password" type="text">
+		<div class="row">
+			<div class="one-third columns">&nbsp;</div>
+			<div class="one-third columns">
+				<label for="admin-password">Admin Password:</label>
+				<input name="admin-password" type="text">
+			</div>
+			<div class="one-third columns">&nbsp;</div>
+		</div>
 		<?php echo br(1); ?>
 		<input class="button-primary email-button" type="submit" value="Submit Email">
 	</div>
