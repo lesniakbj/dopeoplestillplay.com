ad4b555da8cdb20b06ee720e8423d5f7b273999c
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-08-01 09:57:50 -0500

    Fixed some wonky formatting

--- public_html/igniter/application/views/statics/admin_home.php
+++ public_html/igniter/application/views/statics/admin_home.php
@@ -1,23 +1,21 @@
 <form name="admin-login" class="" action="" method="post">
 	<div class="row">
-		<div class="row">
-			<div class="one-third columns">&nbsp;</div>
-			<div class="one-third columns">
-				<label "for="admin-username">Admin Username:</label>
-				<input name="admin-username" type="text">
-			</div>
-			<div class="one-third columns">&nbsp;</div>
+		<div class="four columns">&nbsp;</div>
+		<div class="four columns">
+			<label "for="admin-username">Admin Username:</label>
+			<input name="admin-username" type="text">
 		</div>
-		<?php echo br(1); ?>
-		<div class="row">
-			<div class="one-third columns">&nbsp;</div>
-			<div class="one-third columns">
-				<label for="admin-password">Admin Password:</label>
-				<input name="admin-password" type="text">
-			</div>
-			<div class="one-third columns">&nbsp;</div>
-		</div>
-		<?php echo br(1); ?>
-		<input class="button-primary email-button" type="submit" value="Submit Email">
+		<div class="four columns">&nbsp;</div>
 	</div>
+	<?php echo br(1); ?>
+	<div class="row">
+		<div class="four columns">&nbsp;</div>
+		<div class="four columns">
+			<label for="admin-password">Admin Password:</label>
+			<input name="admin-password" type="text">
+		</div>
+		<div class="four columns">&nbsp;</div>
+	</div>
+	<?php echo br(1); ?>
+	<input class="button-primary email-button" type="submit" value="Submit Email">
 </form>
