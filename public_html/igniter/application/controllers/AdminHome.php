6227b57de99d99a1a022af82008d8c9e32a5fe29
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-08-01 09:49:08 -0500

    Added work to log, added admin view and controller. Pushing the test view now.

---
+++ public_html/igniter/application/controllers/AdminHome.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class AdminHome extends CI_Controller {
+	public function index() {		
+		$data['title'] = 'Administration';
+		$data['css'] = 'statics/home.css';
+		
+		$this->load->view('templates/header', $data);
+		$this->load->view('statics/admin_home', $data);
+		$this->load->view('templates/footer', $data);
+	}
+}
---
+++ public_html/igniter/application/views/statics/admin_home.php
@@ -0,0 +1,12 @@
+<form name="admin-login" class="" action="" method="post">
+	<div class="row">
+   		<div class="one-third columns">
+			<label for="admin-username">Admin Username:</label>
+			<input name="admin-username" type="text">
+			<?php echo br(1); ?>
+			<label for="admin-password">Admin Password:</label>
+			<input name="admin-password" type="text">
+			<input class="button-primary email-button" type="submit" value="Submit Email">
+		</div>
+	</div>
+</form>
