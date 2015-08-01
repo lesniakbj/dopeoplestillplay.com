6743f77617ef7a8a4f19a4095902b9b9f2fd57df
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-08-01 10:56:32 -0500

    Add all files

--- public_html/igniter/application/controllers/AdminHome.php
+++ public_html/igniter/application/controllers/AdminHome.php
@@ -12,12 +12,18 @@ class AdminHome extends CI_Controller {
 	}
 	
 	public function login() {
-		$loginData = $this->input->post('admin-login');
+		$loginData = $this->input->post('admin-login', TRUE));
 		
 		if($loginData['admin-username'] = 'lesniakbj' && $loginData['admin-password'] = 'Fgoal1313_') {
 			$this->load->view('templates/header', $data);
 			//$this->load->view('statics/admin_home', $data);
 			$this->load->view('templates/footer', $data);
+			
+			// If we logged in, create a session and check it in the view?... Redirect to a page?...
+			$this->load->helper('url');
+			$this->output->set_header('url=/');
+		} else {
+			
 		}
 	}
 }
