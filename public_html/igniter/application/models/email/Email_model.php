5e800131ca942293631fe746291a76eac9ae456f
Brendan Lesniak <blesniak@denniskirk.com>
2015-08-03 12:19:32 -0500

    Changed the Email_Model to correctly use the active record databse query type. Allows interchangeability between backends

--- public_html/igniter/application/models/email/Email_model.php
+++ public_html/igniter/application/models/email/Email_model.php
@@ -7,9 +7,12 @@ class Email_model extends CI_Model {
 	}
 	
 	public function submitEmailToDatabase($email) {
-		$sql = "INSERT INTO user_email_list (email_address) VALUES(".$this->db->escape($email).")";
-		$query = $this->db->query($sql);
+		$emailData = array(
+			'email_address' => $this->db->escape($email)
+		);	
+		$querySuccess = $this->db->insert('user_email_list', $emailData);
 		
-		return ($this->db->affected_rows() != 1) ? false : true;
+		echo($querySuccess);
+		return $querySuccess;
 	}
 }
