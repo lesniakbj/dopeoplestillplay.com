4f238280cfc0d3dd91a80974fbb28632444b6642
Brendan Lesniak <blesniak@denniskirk.com>
2015-08-03 11:59:25 -0500

    Missed ; in Home.php

--- public_html/igniter/application/controllers/Home.php
+++ public_html/igniter/application/controllers/Home.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 	public function index() {		
-		$data['title'] = 'Home'
+		$data['title'] = 'Home';
 		$data['css'] = 'statics/home.css';
 	
 		$this->load->view('templates/header', $data);
