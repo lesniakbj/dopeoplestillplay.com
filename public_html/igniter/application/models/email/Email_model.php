133e81b58e66fc5317d2cfb966ddecae851bcb24
Brendan Lesniak <blesniak@denniskirk.com>
2015-08-03 12:33:00 -0500

    Removed unnessary escapes, the data was already escaped when put in the query

--- public_html/igniter/application/models/email/Email_model.php
+++ public_html/igniter/application/models/email/Email_model.php
@@ -7,8 +7,9 @@ class Email_model extends CI_Model {
 	}
 	
 	public function submitEmailToDatabase($email) {
+		// Data is automatically escaped, no need to ensure safe escaping
 		$emailData = array(
-			'email_address' => $this->db->escape($email)
+			'email_address' => $email
 		);	
 		$querySuccess = $this->db->insert('user_email_list', $emailData);
 		
