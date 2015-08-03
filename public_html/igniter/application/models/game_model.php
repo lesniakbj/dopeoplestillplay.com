2b5f4f97a064f17e65cb5e3d0bf1ae8e2961b7dc
Brendan Lesniak <blesniak@denniskirk.com>
2015-08-03 11:52:06 -0500

    Added in notes of where the data comes from, and solidified the overall game_model structure

--- public_html/igniter/application/models/game_model.php
+++ public_html/igniter/application/models/game_model.php
@@ -1,13 +1,36 @@
 <?php
+/*
+|	DATA-NOTES:
+|		STEAM SPY:
+|			URL = steamspy.com/api.php?request=all
+|		
+|			Important Data from SteamSpy:
+|				- Appid / Game Name (name, appid)
+|				- Owners of the Game (owners)
+|				- Average 2 weeks Players Count = Average Recent Playercount (players_2weeks)
+|				- Average 2 Weeks Playtime = Average Recent Playtime (average_2weeks)
+|				- Median 2 Weeks Playtime = Median Recent Playtime (median_2weeks)
+|			
+|				"570": {	
+|					"appid":570,
+|					"name":"Dota 2",
+|					"owners":54048858,
+|					"players_2weeks":8670085,
+|					"average_2weeks":1360,
+|					"median_2weeks":745
+|				}
+*/
 class Game_model extends CI_Model {
+	// Table: game_game
 	var $gameTitle   = ''; 				// SteamSpy - Name, Steam - Title, 
 	var $appId = ''; 					// SteamSpy - appid, Steam - appid
-	var $description = ''; 				// 
+	var $description = ''; 				// Steam (most likely)
 	
+	// Table: game_stats
     var $totalOwners = ''; 				// SteamSpy - owners
-	
-	var $averageRecentPlayercount = '';
-	var $averageRecentPlaytime = '';
+	var $averageRecentPlayercount = '';	// SteamSpy - players_2weeks
+	var $averageRecentPlaytime = '';	// SteamSpy - average_2weeks
+	var $mediaRecentPlaytime ='';		// SteamSpy - median_2weeks
 	
 	public function __construct() {
 		parent::__construct();
@@ -27,23 +50,9 @@ class Game_model extends CI_Model {
 		
 		return "Query Failed - No game data!";
 	}
-}
-/*
-	URL = steamspy.com/api.php?request=all
 	
-	Important Data from SteamSpy:
-		- Appid / Game Name (name, appid)
-		- Owners of the Game (owners)
-		- Average 2 weeks Players Count = Average Recent Playercount (players_2weeks)
-		- Average 2 Weeks Playtime = Average Recent Playtime (average_2weeks)
-		- Median 2 Weeks Playtime = Median Recent Playtime (median_2weeks)
-		
-		"570": {	
-			"appid":570,
-			"name":"Dota 2",
-			"owners":54048858,
-			"players_2weeks":8670085,
-			"average_2weeks":1360,
-			"median_2weeks":745
-		}
-*/
+	public function insertNewGameData() { 
+		// Set the POST/GET data to the class variables
+		// then use that model to insert to the DB (db->insert())
+	}
+}
