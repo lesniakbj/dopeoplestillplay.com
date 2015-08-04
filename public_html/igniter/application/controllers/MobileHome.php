05b6ae8424c67d6b9e95b182fde3a7852cfcbf3c
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-08-04 12:16:42 -0400

    Fixed subdomain routes and rewrites. Now able to go to subdomain by accessing /admin, /forums, or /mobile.

--- public_html/igniter/application/controllers/MobileHome.php
+++ public_html/igniter/application/controllers/MobileHome.php
@@ -1,20 +1,20 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class ForumsHome extends CI_Controller {
+class MobileHome extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 	}
 	
 	public function index() {		
-		$data['title'] = 'Forums';
+		$data['title'] = 'Mobile Home';
 		$data['css'] = '';
 		
 		$this->load->view('templates/header', $data);
 		$this->load->view('templates/footer', $data);
 	}
 	
-	public function redirect($url = 'http://forums.dopeoplestillplay.com', $perm = TRUE) {
+	public function redirect($url = 'http://m.dopeoplestillplay.com', $perm = TRUE) {
 		if(!headers_sent()) {
 			header('Location:'.$url, true, ($perm === TRUE) ? 301 : 302);
 		}		
