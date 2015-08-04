8f1975f3b5918b859e86f30e7e3712acef9ec11a
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-08-03 22:00:16 -0500

    Updated footer with new text, will soon be replacing with resource strings

--- public_html/igniter/application/views/templates/footer.php
+++ public_html/igniter/application/views/templates/footer.php
@@ -1,7 +1,7 @@
 			<div class="row">
 				<div class="three columns">&nbsp;</div>
 				<em class="six columns footer-text">
-					&copy;, Brendan Lesniak. Last Updated on <?php echo date("F d Y H:i:s", getlastmod()); ?>
+					&copy; August 2015, Brendan Lesniak. Last Updated on <?php echo date("F d Y H:i:s", getlastmod()); ?>
 				</em>
 				<div class="three columns">&nbsp;</div>
 			</div>
