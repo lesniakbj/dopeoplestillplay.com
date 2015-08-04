a155405cec05111cc96cb2bd6ad3da69eaf0aaab
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-08-04 18:39:17 -0500

    Added banner to admin home

--- public_html/igniter/application/views/statics/admin/admin_home.php
+++ public_html/igniter/application/views/statics/admin/admin_home.php
@@ -2,6 +2,8 @@
 	if($this->session->userdata('logged-in') == FALSE) {
 		show_error('Forbidden content. Please login to view this content.', 403, '403 Forbidden Content');
 	} else {		
-		
+?>
+	<h4>Down here will be a banner until a new tool is chosen</h4>
+<?		
 	} // End session check
 ?>
