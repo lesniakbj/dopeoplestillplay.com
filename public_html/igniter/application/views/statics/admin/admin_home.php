094461adbcd3da05c6b64e2c9116995c25783158
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-08-03 20:35:59 -0500

    Added HTML changes

--- public_html/igniter/application/views/statics/admin/admin_home.php
+++ public_html/igniter/application/views/statics/admin/admin_home.php
@@ -3,8 +3,10 @@
 		$this->output->set_header('url=/');
 	} else {
 ?>
+	<div class="button-primary">Refresh ALL Data Feeds</div>
 	<section class="header">
 		<h4 class="title">And on the homepage...</h4>
+		
 	</section>
 <?
 	} // End session check
