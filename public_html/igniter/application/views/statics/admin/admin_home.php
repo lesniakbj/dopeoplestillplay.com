d0c34f4521c917d858ec5ecc77dd3e7350ec6d94
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-08-04 18:57:43 -0500

    Added some more CSS to the admin_page, changed overall content

--- public_html/igniter/application/views/statics/admin/admin_home.php
+++ public_html/igniter/application/views/statics/admin/admin_home.php
@@ -3,8 +3,8 @@
 		show_error('Forbidden content. Please login to view this content.', 403, '403 Forbidden Content');
 	} else {		
 ?>
-	<div class="container banner-text">
-		<h4>Down here will be a banner until a new tool is chosen</h4>
+	<div class="container tool-area">
+		<span class="tool-banner"><h4>Down here will be a banner until a new tool is chosen</h4>
 	</div>
 <?		
 	} // End session check
