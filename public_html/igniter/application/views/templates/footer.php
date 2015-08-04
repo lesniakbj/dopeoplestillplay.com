6125136bf48b101a779730e795aa6d8fcd45ca16
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-08-03 22:07:05 -0500

    Updated footer with new text, will soon be replacing with resource strings. Changed time strings

--- public_html/igniter/application/views/templates/footer.php
+++ public_html/igniter/application/views/templates/footer.php
@@ -1,7 +1,7 @@
 			<div class="row">
 				<div class="three columns">&nbsp;</div>
 				<em class="six columns footer-text">
-					&copy; August 2015, Brendan Lesniak. Last Updated on <?php echo date("F d Y H:i:s", filemtime("/home/doeplsha/public_html/.")); ?>
+					&copy; August 2015, Brendan Lesniak. Last Updated on <?php echo date("F d Y H:i:s", stat("/home/doeplsha/public_html/.")['mtime']); ?>
 				</em>
 				<div class="three columns">&nbsp;</div>
 			</div>
