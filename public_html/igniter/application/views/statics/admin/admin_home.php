8e041fa5ea55f2fb3e41992b52f008aba0e6a09b
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-08-03 19:56:09 -0500

    Forgot to add new file

---
+++ public_html/igniter/application/views/statics/admin/admin_home.php
@@ -0,0 +1,11 @@
+<?
+	if($this->session->userdata('logged-in') == FALSE {
+		$this->output->set_header('url=http://admin.dopeoplestillplay.com/');
+	} else {
+?>
+	<section class="header">
+		<h4 class="title">You are successfully logged in!</h4>
+	</section>
+<?
+	} // End session check
+?>
