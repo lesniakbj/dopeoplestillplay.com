285056b71eab0cdb288818dec49a70a09f789cef
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-08-04 18:33:00 -0500

    Forgot to rename admin tools controller.

--- public_html/igniter/application/controllers/AdminTools.php
+++ public_html/igniter/application/controllers/AdminTools.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class AdminHome extends CI_Controller {
+class AdminTools extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		$this->load->library('session');	
