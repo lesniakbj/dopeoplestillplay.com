6ba02657a20fea25b3a4d75d481ac2e171bb3b49
Brendan Lesniak <blesniak@denniskirk.com>
2015-08-04 12:41:35 -0500

    Need to figure out these routes later

--- public_html/mobile/index.php
+++ public_html/mobile/index.php
@@ -152,7 +152,7 @@ switch (ENVIRONMENT)
  */
 	// The directory name, relative to the "controllers" folder.  Leave blank
 	// if your controller is not in a sub-folder within the "controllers" folder
-	$routing['directory'] = 'mobile';
+	//$routing['directory'] = 'mobile';
 
 	// The controller class file name.  Example:  mycontroller
 	// $routing['controller'] = '';
