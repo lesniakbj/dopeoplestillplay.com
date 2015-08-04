8b1d5afa95cb2860dd56203c03b93724dfd84bd1
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-08-03 20:58:20 -0500

    Added errors when user not logged in

--- public_html/igniter/application/views/statics/admin/admin_home.php
+++ public_html/igniter/application/views/statics/admin/admin_home.php
@@ -1,11 +1,10 @@
 <?
-	if($this->session->userdata('logged-in') == FALSE) {
-		$this->output->set_header('url=/');
+	if($this->session->userdata('logged-in') !== FALSE) {
+		show_404();
 	} else {
 ?>
 	<section class="header">
-		<h4 class="title">And on the homepage...</h4>
-		
+		<h4 class="title">And on the homepage...</h4>	
 	</section>
 <?
 	} // End session check
