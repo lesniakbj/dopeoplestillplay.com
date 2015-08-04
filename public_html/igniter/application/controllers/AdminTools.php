0e55112b3b819cb084ff00db89c7646de5c6a258
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-08-04 18:37:30 -0500

    Forgot to load the helpers when they are needed.

--- public_html/igniter/application/controllers/AdminTools.php
+++ public_html/igniter/application/controllers/AdminTools.php
@@ -4,6 +4,10 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 class AdminTools extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
+		
+		$this->load->helper('url');
+		$this->load->helper('html');
+		
 		$this->load->library('session');	
 	}
 	
