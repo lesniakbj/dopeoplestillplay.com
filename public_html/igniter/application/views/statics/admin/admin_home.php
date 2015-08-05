c170bd10c3b33d06634e75c3c0a3c85528746e05
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-08-04 19:01:00 -0500

    Fixed text in the banner

--- public_html/igniter/application/views/statics/admin/admin_home.php
+++ public_html/igniter/application/views/statics/admin/admin_home.php
@@ -4,7 +4,7 @@
 	} else {		
 ?>
 	<div class="container tool-area">
-		<span class="tool-banner"><h4>Down here will be a banner until a new tool is chosen</h4></span>
+		<span class="tool-banner">Welcome to the Administrator area! Please choose your tool above to continue.</span>
 	</div>
 <?		
 	} // End session check
