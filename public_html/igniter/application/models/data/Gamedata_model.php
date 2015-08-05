034a47e9846864a87f4827f2cc31112918f96913
Brendan Lesniak <blesniak@denniskirk.com>
2015-08-05 13:35:08 -0500

    updated to correct query structure

--- public_html/igniter/application/models/data/Gamedata_model.php
+++ public_html/igniter/application/models/data/Gamedata_model.php
@@ -89,7 +89,7 @@ class Gamedata_model extends CI_Model {
 		
 		$queryResults = $this->db->get();
 		if( $queryResults->num_rows() > 0) {
-			return $queryResults->results();
+			return $queryResults->result();
 		}
 	}
 }
