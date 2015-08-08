59301794f6d85913d84c3eff10438852eaf51ba0
unknown <brendan.lesniak@gmail.com>
2015-08-08 10:08:57 -0500

    Fixing stupid, and removing test

--- public_html/admin/views/statics/tools/admin_game_information_tool.php
+++ public_html/admin/views/statics/tools/admin_game_information_tool.php
@@ -2,9 +2,7 @@
 	if($this->session->userdata('logged-in') == FALSE) {
 		show_error('Forbidden content. Please login to view this content.', 403, '403 Forbidden Content');
 	} else {
-
-		print_r($platforms);
-	
+		
 		$formAttrs = array(
 			'action' => 'https://www.admin.dopeoplestillplay.com/tools/gameinfo/submit',
 			'class'  => '', 
@@ -14,19 +12,10 @@
 			'method' => 'post'
 		);
 		
+		// Platforms is passed in from the controller
 		echo form_open('tools/gameinfo', $formAttrs);
 		echo form_label('Game Platform:', 'game-platform');
-	
-		// TODO: Change this from static array to list of platforms retrieved from database...
-		$platOptions = array(
-			'pc'  		=> 'PC',
-			'ps3'		=> 'Playstation 3',
-			'ps4'		=> 'Playstation 4',
-			'xbox360' 	=> 'Xbox 360',
-			'xboxone'	=> 'Xbox One'		
-		);
-
-		echo form_dropdown('game-platform', $platOptions, 'pc');
+		echo form_dropdown('game-platform', $platforms, 'pc');
 		echo form_submit('submit-scrape-info','Submit');
 		echo form_close();
 	} // End session check
