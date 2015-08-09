6e33563de13b103607fc7bda24abc9a895483bda
unknown <brendan.lesniak@gmail.com>
2015-08-09 08:54:55 -0500

    Pushing format change

--- public_html/admin/views/statics/admin_home.php
+++ public_html/admin/views/statics/admin_home.php
@@ -1,11 +1,8 @@
 <?
 	if($this->session->userdata('logged-in') == FALSE) {
 		show_error('Forbidden content. Please login to view this content.', 403, '403 Forbidden Content');
-	} else {		
+	}	
 ?>
-	<div class="container tool-area">
-		<h4 class="tool-banner">Welcome to the Administrator area! Please choose your tool above to continue.</h4>
-	</div>
-<?		
-	} // End session check
-?>
+<div class="container tool-area">
+	<h4 class="tool-banner">Welcome to the Administrator area! Please choose your tool above to continue.</h4>
+</div>
--- public_html/admin/views/statics/tools/admin_game_information_tool.php
+++ public_html/admin/views/statics/tools/admin_game_information_tool.php
@@ -1,22 +1,21 @@
 <?
 	if($this->session->userdata('logged-in') == FALSE) {
 		show_error('Forbidden content. Please login to view this content.', 403, '403 Forbidden Content');
-	} else {
+	}
 		
-		$formAttrs = array(
-			'action' => 'https://www.admin.dopeoplestillplay.com/tools/gameinfo/submit',
-			'class'  => '', 
-			'id' 	 => 'gameinformation-tool', 
-			'name'	 => 'gameinformation-tool',
-			'value'  => 'Game Information',
-			'method' => 'post'
-		);
+	$formAttrs = array(
+		'action' => 'https://www.admin.dopeoplestillplay.com/tools/gameinfo/submit',
+		'class'  => '', 
+		'id' 	 => 'gameinformation-tool', 
+		'name'	 => 'gameinformation-tool',
+		'value'  => 'Game Information',
+		'method' => 'post'
+	);
 		
-		// Platforms is passed in from the controller
-		echo form_open('tools/gameinfo/submit', $formAttrs);
-		echo form_label('Game Platform:', 'game-platform');
-		echo form_dropdown('game-platform', $platforms, 'pc');
-		echo form_submit('submit-scrape-info','Submit');
-		echo form_close();
-	} // End session check
+	// Platforms is passed in from the controller
+	echo form_open('tools/gameinfo/submit', $formAttrs);
+	echo form_label('Game Platform:', 'game-platform');
+	echo form_dropdown('game-platform', $platforms, 'pc');
+	echo form_submit('submit-scrape-info','Submit');
+	echo form_close();
 ?>
