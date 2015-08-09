3cfc10403cecd1046a0c113e83a81313192af1c2
unknown <brendan.lesniak@gmail.com>
2015-08-09 08:53:15 -0500

    Fixed the overall structure of admin views. Replicating now.

--- public_html/admin/views/statics/tools/admin_data_tools.php
+++ public_html/admin/views/statics/tools/admin_data_tools.php
@@ -1,33 +1,31 @@
 <?
 	if($this->session->userdata('logged-in') == FALSE) {
 		show_error('Forbidden content. Please login to view this content.', 403, '403 Forbidden Content');
-	} else {		
+	}	
 
-		echo '<div class="container tool-area tools">';
-		echo 	'<div class="tools-header">Admin Data Tools</div>';
-		
-		$toolsMenu = array(
-			anchor('/tools/datatools/all', 'Scrape All Data Feeds', 'title="Scrape Feeds" id="all" class="button"'),
-			anchor('/tools/datatools/steam', 'Scrape Steam Data Feed', 'title="Data Feeds" id="steam" class="button"'),
-			anchor('/tools/datatools/steamspy', 'Scrape SteamSpy Data Feed', 'title="Database" id="steamspy" class="button"'),
-			anchor('/tools/datatools/gameinformation', 'Scrape Game Information Data Feed', 'title="Logs" id="gameinformation" class="button"'),
-		);
+	echo '<div class="container tool-area tools">';
+	echo 	'<div class="tools-header">Admin Data Tools</div>';
+	
+	$toolsMenu = array(
+		anchor('/tools/datatools/all', 'Scrape All Data Feeds', 'title="Scrape Feeds" id="all" class="button"'),
+		anchor('/tools/datatools/steam', 'Scrape Steam Data Feed', 'title="Data Feeds" id="steam" class="button"'),
+		anchor('/tools/datatools/steamspy', 'Scrape SteamSpy Data Feed', 'title="Database" id="steamspy" class="button"'),
+		anchor('/tools/datatools/gameinformation', 'Scrape Game Information Data Feed', 'title="Logs" id="gameinformation" class="button"'),
+	);
 
-		$toolsAttrs = array(
-			'class' => 'admin-actions'
-		);
+	$toolsAttrs = array(
+		'class' => 'admin-actions'
+	);
 
-		echo ul($toolsMenu, $toolsAttrs);
-			
-		echo '</div>';		
-		echo '<div class="container tool-area data-feed">';
-		echo	'<div class="loading-overlay">';
-		echo		'<img src="/images/ajax-loader.gif" alt="Fetching Data..." id="loading-data">';
-		echo	'</div>';
-		echo 	'<div class="tools-header">Data Viewer</div>';
-		echo		'<div class="data-information">';
-		echo			'<div class="data-tool"></div>';
-		echo		'</div>';					
-		echo '</div>';	
-	} // End session check
-?>
+	echo ul($toolsMenu, $toolsAttrs);
+	echo '</div>';
+?>			
+<div class="container tool-area data-feed">
+	<div class="loading-overlay">
+		<img src="/images/ajax-loader.gif" alt="Fetching Data..." id="loading-data">
+	</div>
+	<div class="tools-header">Data Viewer</div>
+	<div class="data-information">
+		<div class="data-tool"></div>
+	</div>					
+</div>	
